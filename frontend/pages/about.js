1f61150535757e62c64035218d87fae3f02ce5aa
Schow94 <Schow94@gmail.com>
2022-10-07 09:57:55 -0700

    Add bios to about page

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,4 +1,4 @@
-<h1>this is the receipts page</h1>;
+import Image from "next/image";
 import Navbar from "../components/navbar";
 
 export default function Login() {
@@ -6,7 +6,94 @@ export default function Login() {
 		<div className="body">
 			<Navbar />
 			<div className="App">
-				<h1>About Us</h1>
+				<div className="about-image-banner"></div>
+				<div className="welcome-text-container">
+					<h1>About Us</h1>
+				</div>
+
+				<div className="about-text-container">
+					<p className="about-text">
+						Founded by a group of OSU students during Oregon State's annual Fall
+						2022 Beaverhacks Hackathon who saw a need for improving the tab
+						splitting process when with friends. The theme for this hackathon
+						was "Life Hacks".
+					</p>
+				</div>
+
+				<h1 className="about-students-title">Who we are</h1>
+
+				<div className="students-container">
+					<div className="student-container">
+						<img
+							className="profile_img"
+							src="/stephen_profile.jpg"
+							alt="stephen_img"
+						/>
+						<div className="student-about-container">
+							<h3>Stephen Chow</h3>
+							<p>
+								Biotechnology professional. 2nd year in Oregon State's CS
+								program.
+							</p>
+						</div>
+					</div>
+
+					<hr />
+
+					<div className="student-container">
+						<img
+							className="profile_img"
+							src="/matt_profile.jpeg"
+							alt="stephen_img"
+						/>
+						<div className="student-about-container">
+							<h3>Matthew Lam</h3>
+							<p>Tax extraordinaire turned Software Engineer</p>
+						</div>
+					</div>
+
+					<hr />
+
+					<div className="student-container">
+						<img
+							className="profile_img"
+							src="/abby_profile.jpeg"
+							alt="stephen_img"
+						/>
+						<div className="student-about-container">
+							<h3>Abby Thornton</h3>
+							<p>
+								I am currently pursing a bachelors degree from Oregon State
+								University in Computer Science. As an administrative
+								professional, I became enamored with the ways in which we can
+								use technology to build custom solutions. I went from managing
+								basic websites and databases to learning how to design custom
+								reports, write automation and build custom power apps in order
+								to solve unique problems and make work more efficient. This has
+								led me to diving deeper into technology and pursuing a new
+								career in which I can design software that will help others
+								create value through technology.
+							</p>
+						</div>
+					</div>
+
+					<hr />
+
+					<div className="student-container">
+						<img
+							className="profile_img"
+							src="/collin_profile.jpeg"
+							alt="stephen_img"
+						/>
+						<div className="student-about-container">
+							<h3>Collin Sidler</h3>
+							<p>
+								CPA turned Computer Science student based in the Orange County,
+								CA area. Final quarter in the program.
+							</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
