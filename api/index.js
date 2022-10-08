8ab6c78c7375207298b6126a642aa1e6c347e726
cjsidler <38596474+cjsidler@users.noreply.github.com>
2022-10-08 10:48:30 -0700

    adds OCR API call with temp sample data

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
+require("dotenv");
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads" });
+const fs = require("fs");
+const Client = require("@veryfi/veryfi-sdk");
+const sample_ocr_response = require("./sample_ocr_response.json");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+
+const client_id = process.env.VERYFI_CLIENT_ID;
+const client_secret = process.env.VERYFI_CLIENT_SECRET;
+const username = process.env.VERYFI_USERNAME;
+const api_key = process.env.VERYFI_APIKEY;
+
 const app = express();
-require("dotenv");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -18,7 +30,7 @@ app.use(bodyParser.json());
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-	res.json({ message: "This is the root route!" });
+    res.json({ message: "This is the root route!" });
 });
 
 /*
@@ -26,6 +38,31 @@ app.get("/", (req, res) => {
 */
 app.use("/users", require("./routes/users"));
 
+/*
+    OCR API
+*/
+
+// app.post("/upload_files", upload.single("file"), async (req, res) => {
+//     const veryfi_client = new Client(client_id, client_secret, username, api_key);
+//     try {
+//         const data = await veryfi_client.process_document(req.file.path);
+//         console.log(data);
+//         res.json(data);
+
+//         // Remove temporary file
+//         fs.unlink(req.file.path, (err) => {
+//             if (err) console.log(err);
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         res.json("error calling OCR API");
+//     }
+// });
+
+app.post("/upload_files", async (req, res) => {
+    res.json(sample_ocr_response);
+});
+
 app.listen(PORT, () => {
-	console.log(`Example app listening on port ${PORT}`);
+    console.log(`Example app listening on port ${PORT}`);
 });
