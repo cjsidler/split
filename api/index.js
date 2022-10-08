81d305745b7e6a16a65b41f2c5acd67ebfc6ef85
Matthew Lam <lamma@engr.orst.edu>
2022-10-07 23:10:17 -0400

    basic express setup

---
+++ api/index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+const port = 3000
+ 
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })  
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
