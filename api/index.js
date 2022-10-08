e23ef9124f869cc1b653afec3c7c60c2885aba54
Matthew Lam <lamma@engr.orst.edu>
2022-10-08 00:24:21 -0400

    working connection to mongodb

--- api/index.js
+++ api/index.js
@@ -1,18 +1,39 @@
-var express = require("express");
-var cors = require("cors");
-var app = express();
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+
+import mongoose from 'mongoose';
+import Blog from './Model/Blog.js';
 
 /*
   CONSTANTS
 */
 const PORT = 8080;
 
-app.use(cors());
+mongoose.connect("mongodb+srv://mattlamDB:SaWzAYjznnpjiglH@cluster0.dlleb.mongodb.net/?retryWrites=true&w=majority")
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+// app.use(cors());
 
-app.listen(PORT, () => {
-	console.log(`Example app listening on port ${PORT}`);
+// Create a new blog post object
+const article = new Blog({
+    title: 'Awesome Post!',
+    slug: 'awesome-post',
+    published: true,
+    content: 'This is the best post ever',
+    tags: ['featured', 'announcement'],
 });
+  
+// Insert the article in our MongoDB database
+await article.save();
+
+// Find a single blog post
+// const firstArticle = await Blog.findOne({});
+// console.log(firstArticle);
+
+// app.get("/", (req, res) => {
+// 	res.send("Hello World!");
+// });
+
+// app.listen(PORT, () => {
+// 	console.log(`Example app listening on port ${PORT}`);
+// });
