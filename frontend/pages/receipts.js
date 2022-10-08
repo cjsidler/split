9f8721d29ce67e442937e54894372179509467ba
Matthew Lam <lamma@engr.orst.edu>
2022-10-08 11:53:14 -0400

    added styling sheet for receipts page

--- frontend/pages/receipts.js
+++ frontend/pages/receipts.js
@@ -1,12 +1,15 @@
 import Navbar from "../components/navbar";
 import ReceiptTable from "../components/ReceiptTable";
+import styles from "./receipts.module.scss";
 
-export default function Login() {
+export default function Receipts() {
 	return (
 		<div className="body">
 			<Navbar />
 			<div className="App">
-				<ReceiptTable />
+				<div className={styles.table_container}>
+					<ReceiptTable className={styles.table_parent}/>
+				</div>
 			</div>
 		</div>
 	);
