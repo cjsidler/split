7a881b36d2b5142b7da2ea77b43aaa94e0a5daf2
Collin <cjsidler@gmail.com>
2022-10-07 23:23:16 -0700

    working OCR API call

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer({ dest: "uploads" });
+const Client = require("@veryfi/veryfi-sdk");
+
+const client_id = process.env.VERYFI_CLIENT_ID;
+const client_secret = process.env.VERYFI_CLIENT_SECRET;
+const username = process.env.VERYFI_USERNAME;
+const api_key = process.env.VERYFI_APIKEY;
 
 const app = express();
 
@@ -11,13 +20,13 @@ const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 /*
   ROOT ROUTE
 */
 app.get("/", (req, res) => {
-	res.json({ message: "This is the root route" });
+    res.json({ message: "This is the root route" });
 });
 
 /*
@@ -25,6 +34,18 @@ app.get("/", (req, res) => {
 */
 app.use("/users", require("./routes/users"));
 
+app.post("/upload_files", upload.single("file"), async (req, res) => {
+    const veryfi_client = new Client(client_id, client_secret, username, api_key);
+    try {
+        const data = await veryfi_client.process_document(req.file.path);
+        console.log(data);
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.json("error calling OCR API");
+    }
+});
+
 app.listen(PORT, () => {
-	console.log(`Example app listening on port ${PORT}`);
+    console.log(`Example app listening on port ${PORT}`);
 });
