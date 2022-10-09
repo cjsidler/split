542121c29e912924dd38aade18e5937a8a1efaff
cjsidler <38596474+cjsidler@users.noreply.github.com>
2022-10-09 13:07:24 -0700

    styling updates for addreceipt

--- frontend/pages/addreceipt.js
+++ frontend/pages/addreceipt.js
@@ -8,7 +8,7 @@ import styles from "./addreceipt.module.scss";
 
 import data from "../data/result.json";
 
-export default function Login() {
+export default function AddReceipt() {
     const isLoggedIn = useStore((state) => state.isLoggedIn);
     const router = useRouter();
 
@@ -16,10 +16,9 @@ export default function Login() {
         return data.line_items.map((item) => {
             return (
                 <div className={styles.item_container}>
-                    <p>Price: {item.total}</p>
-                    <p>Item: {item.description}</p>
-                    <p>Quantity: {item.quantity}</p>
-                    <p>Type: {item.type}</p>
+                    <p style={{ flexBasis: "50%" }}>Item: {item.description}</p>
+                    <p style={{ flexBasis: "15%" }}>Quantity: {item.quantity}</p>
+                    <p style={{ flexBasis: "15%" }}>Price: {item.total}</p>
                 </div>
             );
         });
@@ -54,32 +53,27 @@ export default function Login() {
 
                     <div className={styles.receipt_container}>
                         <div className={styles.vendor_container}>
-                            <img src={data.img_thumbnail_url} alt="image" />
+                            <img src={data.img_url} alt="image" />
                             <p>{data.vendor.name}</p>
+                            <p>{data.vendor.address}</p>
                             <p>{data.vendor.phone_number}</p>
-                            <p>{data.vendor.web}</p>
-                            <p>{data.vendor_type}</p>
+                        </div>
+
+                        <p>Date: {data.date}</p>
+
+                        <h1>Items</h1>
+                        {renderItems()}
+
+                        <div className={styles.totals_container}>
+                            <p>Tax: {data.tax}</p>
+                            <p>Subtotal: {data.subtotal}</p>
+                            <p>Tip: {data.tip}</p>
+                            <p>Total: {data.total}</p>
                         </div>
 
                         <button className="save-receipt-btn" onClick={saveReceipt}>
                             Save Receipt
                         </button>
-
-                        <p>Payment: {data.payment_display_name}</p>
-                        <p>cashback: {data.cashback}</p>
-                        <p>category: {data.category}</p>
-                        <p>cashback: {data.cashback}</p>
-                        <p>created: {data.created}</p>
-                        <p>date: {data.date}</p>
-                        <p>invoice_number: {data.invoice_number}</p>
-                        <p>phone_number: {data.phone_number}</p>
-                        <p>subtotal: {data.subtotal}</p>
-                        <p>tax: {data.tax}</p>
-                        <p>total: {data.total}</p>
-
-                        <h1>Items</h1>
-
-                        {renderItems()}
                     </div>
                 </div>
             )}
