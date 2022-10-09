e8a9694dd099a2117e928d33e56eaf3cc3f2a4e4
cjsidler <38596474+cjsidler@users.noreply.github.com>
2022-10-09 13:57:40 -0700

    global state for new receipt wip

--- frontend/pages/upload.js
+++ frontend/pages/upload.js
@@ -12,91 +12,90 @@ import Unauthorized from "../components/Unauthorized";
 import styles from "./upload.module.scss";
 
 export default function Upload() {
-	const isLoggedIn = useStore((state) => state.isLoggedIn);
-	const router = useRouter();
+    const isLoggedIn = useStore((state) => state.isLoggedIn);
+    const setReceiptData = useStore((state) => state.setReceiptData);
+    const router = useRouter();
 
-	const [file, setFile] = useState();
-	const [fileChosen, setFileChosen] = useState(false);
+    const [file, setFile] = useState();
+    const [fileChosen, setFileChosen] = useState(false);
 
-	const handleChange = (e) => {
-		setFile(e.target.files[0]);
-		setFileChosen(true);
-	};
+    const handleChange = (e) => {
+        setFile(e.target.files[0]);
+        setFileChosen(true);
+    };
 
-	/*
+    /*
 		Check credentials
 	*/
-	const getCredentials = () => {
-		return localStorage.getItem("username");
-	};
+    const getCredentials = () => {
+        return localStorage.getItem("username");
+    };
 
-	// useEffect(() => {
+    // useEffect(() => {
 
-	// }, []);
+    // }, []);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-		const formData = new FormData();
-		formData.append("file", file);
+        const formData = new FormData();
+        formData.append("file", file);
 
-		const response = await axios({
-			method: "post",
-			url: "http://localhost:8080/upload_files",
-			data: formData,
-			headers: {
-				"Content-Type": "multipart/form-data",
-				Authorization: `Bearer ${localStorage.getItem("token")}`,
-			},
-		});
+        console.log(localStorage.getItem("token"));
 
-		console.log(response);
+        const response = await axios({
+            method: "post",
+            url: "http://localhost:8080/upload_files",
+            data: formData,
+            headers: {
+                "Content-Type": "multipart/form-data",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
 
-		router.push("/addreceipt");
-	};
+        console.log(response);
+        setReceiptData(response);
 
-	return (
-		<div className="container">
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+        router.push("/addreceipt");
+    };
 
-			<div className="body">
-				<Navbar />
-				<div className={styles.upload_screen_container}>
-					{!isLoggedIn && <Unauthorized />}
-					{isLoggedIn && (
-						<form className={styles.upload_form}>
-							{fileChosen ? (
-								<div className={styles.selected_file_container}>
-									<h1>Selected File: </h1>
-									<p>{file.name}</p>
-									<button
-										onClick={handleSubmit}
-										type="submit"
-										className={styles.upload_btn}>
-										Upload Receipt
-									</button>
-								</div>
-							) : (
-								<label
-									htmlFor="file-upload"
-									className={styles.custom_file_upload}>
-									<input
-										id="file-upload"
-										className={styles.pick_file_btn}
-										type="file"
-										onChange={handleChange}
-									/>
-									<BsCloudUpload className={styles.upload_icon} />
-									Choose a Receipt
-								</label>
-							)}
-						</form>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="container">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <div className="body">
+                <Navbar />
+                <div className={styles.upload_screen_container}>
+                    {!isLoggedIn && <Unauthorized />}
+                    {isLoggedIn && (
+                        <form className={styles.upload_form}>
+                            {fileChosen ? (
+                                <div className={styles.selected_file_container}>
+                                    <h1>Selected File: </h1>
+                                    <p>{file.name}</p>
+                                    <button onClick={handleSubmit} type="submit" className={styles.upload_btn}>
+                                        Upload Receipt
+                                    </button>
+                                </div>
+                            ) : (
+                                <label htmlFor="file-upload" className={styles.custom_file_upload}>
+                                    <input
+                                        id="file-upload"
+                                        className={styles.pick_file_btn}
+                                        type="file"
+                                        onChange={handleChange}
+                                    />
+                                    <BsCloudUpload className={styles.upload_icon} />
+                                    Choose a Receipt
+                                </label>
+                            )}
+                        </form>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
