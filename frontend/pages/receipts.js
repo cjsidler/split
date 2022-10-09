9931a4f4cbb0b4c2c8e154b324ae3344c1d5aa8e
Schow94 <Schow94@gmail.com>
2022-10-09 08:45:05 -0700

    Connect receipts component to api

--- frontend/pages/receipts.js
+++ frontend/pages/receipts.js
@@ -1,18 +1,38 @@
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
 import Navbar from "../components/navbar";
 import ReceiptTable from "../components/ReceiptTable";
 
 import styles from "./receipts.module.scss";
 
-
 export default function Receipts() {
+	const [receipts, setReceipts] = useState([]);
+
+	const getReceipts = async () => {
+		const response = await axios({
+			method: "get",
+			url: "http://localhost:8080/receipts",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+		});
+
+		setReceipts(response.data);
+	};
+
+	useEffect(() => {
+		getReceipts();
+	}, []);
+
 	return (
 		<div className="body">
 			<Navbar />
 			<div className="App">
 				<div className={styles.table_container}>
-					<ReceiptTable />
+					<ReceiptTable receipts={receipts} />
 				</div>
-
 			</div>
 		</div>
 	);
