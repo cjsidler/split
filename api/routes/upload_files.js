e22c480a10805e7b5db28738be7b4b7d9bb03a96
Matthew Lam <lamma@engr.orst.edu>
2022-10-09 15:06:24 -0400

    Added back OCR API

--- api/routes/upload_files.js
+++ api/routes/upload_files.js
@@ -27,7 +27,26 @@ const isLoggedIn = async (req, res, next) => {
 	}
 };
 
-// OCR API - Post request
+// OCR API
+// Commented code below is to save unnecessary requests to the API as we are limited to 50 requests total
+
+// router.post("/", isLoggedIn, upload.single("file"), async (req, res) => {
+//     const veryfi_client = new Client(client_id, client_secret, username, api_key);
+//     try {
+//         const response = await veryfi_client.process_document(req.file.path);
+//         console.log(response.data);
+//         res.status(200).send(response.data);
+//     } catch (error) {
+//         console.log(error);
+//         res.json("error calling OCR API");
+//     }
+
+//     // Remove temporary file
+//     fs.unlink(req.file.path, (err) => {
+//         if (err) console.log(err);
+//     });
+// });
+
 router.post("/", isLoggedIn, async (req, res) => {
 	res.json(sample_ocr_response);
 });
