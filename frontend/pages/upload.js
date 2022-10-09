909d0c24e43c500d15d374cf51c60d2cb7d26a6f
cjsidler <38596474+cjsidler@users.noreply.github.com>
2022-10-09 11:02:58 -0700

    protects upload page if user not logged in

--- frontend/pages/upload.js
+++ frontend/pages/upload.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import FormData from "form-data";
+import useStore from "../store/store";
 
 import axios from "axios";
 
@@ -9,86 +10,85 @@ import Navbar from "../components/navbar";
 import styles from "./upload.module.scss";
 
 export default function Upload() {
-	const router = useRouter();
+    const isLoggedIn = useStore((state) => state.isLoggedIn);
+    const router = useRouter();
 
-	const [file, setFile] = useState();
-	const [fileChosen, setFileChosen] = useState(false);
+    const [file, setFile] = useState();
+    const [fileChosen, setFileChosen] = useState(false);
 
-	const handleChange = (e) => {
-		setFile(e.target.files[0]);
-		setFileChosen(true);
-	};
+    const handleChange = (e) => {
+        setFile(e.target.files[0]);
+        setFileChosen(true);
+    };
 
-	/*
+    /*
 		Check credentials
 	*/
-	const getCredentials = () => {
-		return localStorage.getItem("username");
-	};
+    const getCredentials = () => {
+        return localStorage.getItem("username");
+    };
 
-	// useEffect(() => {
+    // useEffect(() => {
 
-	// }, []);
+    // }, []);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-		const formData = new FormData();
-		formData.append("file", file);
+        const formData = new FormData();
+        formData.append("file", file);
 
-		const response = await axios({
-			method: "post",
-			url: "http://localhost:8080/upload_files",
-			data: formData,
-			headers: {
-				"Content-Type": "multipart/form-data",
-				Authorization: `Bearer ${localStorage.getItem("token")}`,
-			},
-		});
+        const response = await axios({
+            method: "post",
+            url: "http://localhost:8080/upload_files",
+            data: formData,
+            headers: {
+                "Content-Type": "multipart/form-data",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
 
-		console.log(response);
+        console.log(response);
 
-		router.push("/addreceipt");
-	};
+        router.push("/addreceipt");
+    };
 
-	return (
-		<div className="container">
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+    return (
+        <div className="container">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-			<div className="body">
-				<Navbar />
-				<div className={styles.upload_screen_container}>
-					<form className={styles.upload_form}>
-						{fileChosen ? (
-							<div className={styles.selected_file_container}>
-								<h1>Selected File: </h1>
-								<p>{file.name}</p>
-								<button
-									onClick={handleSubmit}
-									type="submit"
-									className={styles.upload_btn}>
-									Upload Receipt
-								</button>
-							</div>
-						) : (
-							<label
-								htmlFor="file-upload"
-								className={styles.custom_file_upload}>
-								<input
-									id="file-upload"
-									className={styles.pick_file_btn}
-									type="file"
-									onChange={handleChange}
-								/>
-								Choose a Receipt
-							</label>
-						)}
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+            <div className="body">
+                <Navbar />
+                <div className={styles.upload_screen_container}>
+                    {!isLoggedIn && <button onClick={() => router.push("/login")}>Log-in</button>}
+                    {isLoggedIn && (
+                        <form className={styles.upload_form}>
+                            {fileChosen ? (
+                                <div className={styles.selected_file_container}>
+                                    <h1>Selected File: </h1>
+                                    <p>{file.name}</p>
+                                    <button onClick={handleSubmit} type="submit" className={styles.upload_btn}>
+                                        Upload Receipt
+                                    </button>
+                                </div>
+                            ) : (
+                                <label htmlFor="file-upload" className={styles.custom_file_upload}>
+                                    <input
+                                        id="file-upload"
+                                        className={styles.pick_file_btn}
+                                        type="file"
+                                        onChange={handleChange}
+                                    />
+                                    Choose a Receipt
+                                </label>
+                            )}
+                        </form>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
