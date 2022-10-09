1ae9259b0f56828ee0712bc264d8a606f7830bbb
cjsidler <38596474+cjsidler@users.noreply.github.com>
2022-10-08 22:13:13 -0700

    global state implementation wip

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,33 +1,43 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import Navbar from "../components/navbar";
+import useStore from "../store/store";
 
 export default function Home() {
-	// Store current user in state here
+    // Store current user in state here
+    const isLoggedIn = useStore((state) => state.isLoggedIn);
+    const setIsLoggedIn = useStore((state) => state.setIsLoggedIn);
 
-	return (
-		<div className="container">
-			<Head>
-				<title>Split</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+    useEffect(() => {
+        // Check if localStorage has token that hasn't expired yet
+        // const exp = localStorage.getItem("exp");
+        // localStorage.removeItem("myCat");
+        // If token is expired, clear out token from localStorage and don't set set isLoggedIn to true
+    }, []);
 
-			{/* <main>
+    return (
+        <div className="container">
+            <Head>
+                <title>Split</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            {/* <main>
 				<App />
 			</main> */}
 
-			<div className="body">
-				<Navbar />
-				<div className="App">
-					<div className="landing-image-banner"></div>
-					<div className="welcome-text-container">
-						<h1>Welcome to Split.</h1>
-						<p className="landing-text">
-							Automating the tabsplitting process so you can focus on the
-							important things
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            <div className="body">
+                <Navbar />
+                <div className="App">
+                    <div className="landing-image-banner"></div>
+                    <div className="welcome-text-container">
+                        <h1>Welcome to Split.</h1>
+                        <p className="landing-text">
+                            Automating the tabsplitting process so you can focus on the important things
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
