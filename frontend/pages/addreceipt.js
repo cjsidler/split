fdf7455fa609da24570510ae02540b814d6e5834
cjsidler <38596474+cjsidler@users.noreply.github.com>
2022-10-09 13:07:24 -0700

    styling updates for addreceipt

--- frontend/pages/addreceipt.js
+++ frontend/pages/addreceipt.js
@@ -9,50 +9,50 @@ import styles from "./addreceipt.module.scss";
 import data from "../data/result.json";
 
 export default function Login() {
-	const isLoggedIn = useStore((state) => state.isLoggedIn);
-	const router = useRouter();
+    const isLoggedIn = useStore((state) => state.isLoggedIn);
+    const router = useRouter();
 
-	const renderItems = () => {
-		return data.line_items.map((item) => {
-			return (
-				<div className={styles.item_container}>
-					<p>Price: {item.total}</p>
-					<p>Item: {item.description}</p>
-					<p>Quantity: {item.quantity}</p>
-					<p>Type: {item.type}</p>
-				</div>
-			);
-		});
-	};
+    const renderItems = () => {
+        return data.line_items.map((item) => {
+            return (
+                <div className={styles.item_container}>
+                    <p>Price: {item.total}</p>
+                    <p>Item: {item.description}</p>
+                    <p>Quantity: {item.quantity}</p>
+                    <p>Type: {item.type}</p>
+                </div>
+            );
+        });
+    };
 
-	const saveReceipt = async (e) => {
-		e.preventDefault();
+    const saveReceipt = async (e) => {
+        e.preventDefault();
 
-		const response = await axios({
-			method: "post",
-			url: "http://localhost:8080/receipts",
-			data: data,
-			headers: {
-				Authorization: `Bearer ${localStorage.getItem("token")}`,
-			},
-		});
+        const response = await axios({
+            method: "post",
+            url: "http://localhost:8080/receipts",
+            data: data,
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
 
-		router.push("/receipts");
-	};
+        router.push("/receipts");
+    };
 
-	return (
-		<div className="body">
-			<Navbar />
-			{!isLoggedIn && (
-				<div className={styles.error_container}>
-					<Unauthorized />
-				</div>
-			)}
-			{isLoggedIn && (
-				<div className="App">
-					<h1 className="login-title">Review Receipt before saving</h1>
+    return (
+        <div className="body">
+            <Navbar />
+            {!isLoggedIn && (
+                <div className={styles.error_container}>
+                    <Unauthorized />
+                </div>
+            )}
+            {isLoggedIn && (
+                <div className="App">
+                    <h1 className="login-title">Review Receipt before saving</h1>
 
-					{/* <div className={styles.receipt_container}>
+                    {/* <div className={styles.receipt_container}>
 						<div className={styles.vendor_container}>
 							<img src={data.img_thumbnail_url} alt="image" />
 							<p>{data.vendor.name}</p>
@@ -78,25 +78,25 @@ export default function Login() {
 						<p>total: {data.total}</p>
  */}
 
-					<div className={styles.receipt_container}>
-						<div className={styles.vendor_container}>
-							<img src={data.img_thumbnail_url} alt="image" />
-							<p>{data.vendor.name}</p>
-							<p>{data.vendor.address}</p>
-						</div>
+                    <div className={styles.receipt_container}>
+                        <div className={styles.vendor_container}>
+                            <img src={data.img_thumbnail_url} alt="image" />
+                            <p>{data.vendor.name}</p>
+                            <p>{data.vendor.address}</p>
+                        </div>
 
-						<p>date: {data.date}</p>
-						<p>subtotal: {data.subtotal}</p>
-						<p>tax: {data.tax}</p>
-						<p>total: {data.total}</p>
+                        <p>date: {data.date}</p>
+                        <p>subtotal: {data.subtotal}</p>
+                        <p>tax: {data.tax}</p>
+                        <p>total: {data.total}</p>
 
-						<h1>Items</h1>
+                        <h1>Items</h1>
 
-						{renderItems()}
-					</div>
-				</div>
-				// </div>
-			)}
-		</div>
-	);
+                        {renderItems()}
+                    </div>
+                </div>
+                // </div>
+            )}
+        </div>
+    );
 }
